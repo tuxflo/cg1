5855eadb68aa8547b4aeb39e9f522aff72c91980
tuxflo <flo.hennig@gmail.com>
2015-10-14 14:55:24 +0200

    working version with layout, colored cube, plane

--- main.cpp
+++ main.cpp
@@ -30,43 +30,34 @@ GLuint wheelarray;
 GLuint cubearray;
 
 void create_wheel(float radius);
-void create_cube();
-void create_grid();
+void create_cube(GLuint MyShader);
+void create_grid(GLuint MyShader);
 
 void display()
 {
     //white background
     glClearColor(1.0, 1.0, 1.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // Camera matrix
     cam.calculatePositions();
     glm::mat4 Projection = cam.getProjectionMatrix();
     glm::mat4 View  = cam.getViewMatrix();    // Model matrix : an identity matrix (model will be at the origin)
-    glm::mat4 MVP        = Projection * View * Wheel_Model;
+    Cube_Model = glm::translate(glm::mat4(1.0), glm::vec3(0.0, 1.5, 0.0));
+    glm::mat4 MVP        = Projection * View * Cube_Model;
 
     //pass updated model to the shader (wheel)
     GLuint MatrixID = glGetUniformLocation(program, "MVP");
     glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-    glBindVertexArray(VAOs[wheel]);
-    //Draw the circle
-    glDrawArrays(GL_LINE_LOOP, 0, 1000);
-    //Draw the lines
-    glDrawArrays(GL_LINES, 1000, 1000);
 
     glBindVertexArray(VAOs[cube]);
 
-    Cube_Model = glm::translate(glm::mat4(1.0), glm::vec3(3.0, 0.0, 0.0));
-    MVP = Projection * View * Cube_Model; // Remember, matrix multiplication is the other way around
 
-    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
     glDrawArrays(GL_TRIANGLES, 0, 12*3);
 
     glBindVertexArray(VAOs[grid]);
-    MVP = Projection * View * glm::mat4(1.0); // Remember, matrix multiplication is the other way around
+    MVP = Projection * View * glm::mat4(1.0);
     glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
-    //glDrawArrays(GL_TRIANGLES, 0, 3*2);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     glFlush();
     glutSwapBuffers();
@@ -85,6 +76,11 @@ void resize(int w, int h)
 
 void init()
 {
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
+
     lastTime = glutGet(GLUT_ELAPSED_TIME);
     //Load shader files
     ShaderInfo shaders[] = {
@@ -97,8 +93,8 @@ void init()
 
 
     create_wheel(2.0);
-    create_cube();
-    create_grid();
+    create_cube(shaders[0].shader);
+    create_grid(shaders[0].shader);
 
     glutWarpPointer(1024/2, 768/2);
 }
@@ -122,7 +118,7 @@ void keyboard(unsigned char key, int x, int y)
     }
 }
 
-void create_cube()
+void create_cube(GLuint MyShader)
 {
     static const GLfloat g_vertex_buffer_data[] = {
         -1.0f,-1.0f,-1.0f, // triangle 1 : begin
@@ -173,6 +169,66 @@ void create_cube()
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+
+    // One color for each vertex. They were generated randomly.
+    static const GLfloat g_color_buffer_data[] = {
+        0.583f,  0.771f,  0.014f,
+        0.609f,  0.115f,  0.436f,
+        0.327f,  0.483f,  0.844f,
+        0.822f,  0.569f,  0.201f,
+        0.435f,  0.602f,  0.223f,
+        0.310f,  0.747f,  0.185f,
+        0.597f,  0.770f,  0.761f,
+        0.559f,  0.436f,  0.730f,
+        0.359f,  0.583f,  0.152f,
+        0.483f,  0.596f,  0.789f,
+        0.559f,  0.861f,  0.639f,
+        0.195f,  0.548f,  0.859f,
+        0.014f,  0.184f,  0.576f,
+        0.771f,  0.328f,  0.970f,
+        0.406f,  0.615f,  0.116f,
+        0.676f,  0.977f,  0.133f,
+        0.971f,  0.572f,  0.833f,
+        0.140f,  0.616f,  0.489f,
+        0.997f,  0.513f,  0.064f,
+        0.945f,  0.719f,  0.592f,
+        0.543f,  0.021f,  0.978f,
+        0.279f,  0.317f,  0.505f,
+        0.167f,  0.620f,  0.077f,
+        0.347f,  0.857f,  0.137f,
+        0.055f,  0.953f,  0.042f,
+        0.714f,  0.505f,  0.345f,
+        0.783f,  0.290f,  0.734f,
+        0.722f,  0.645f,  0.174f,
+        0.302f,  0.455f,  0.848f,
+        0.225f,  0.587f,  0.040f,
+        0.517f,  0.713f,  0.338f,
+        0.053f,  0.959f,  0.120f,
+        0.393f,  0.621f,  0.362f,
+        0.673f,  0.211f,  0.457f,
+        0.820f,  0.883f,  0.371f,
+        0.982f,  0.099f,  0.879f
+    };
+
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+    // 2nd attribute buffer : colors
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glVertexAttribPointer(
+        1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+        3,                                // size
+        GL_FLOAT,                         // type
+        GL_FALSE,                         // normalized?
+        0,                                // stride
+        (void*)0                          // array buffer offset
+    );
+
+    glBindAttribLocation(MyShader, 0, "pos");
+    glBindAttribLocation(MyShader, 1, "Incolor");
 }
 
 void create_wheel(float radius)
@@ -206,7 +262,7 @@ void create_wheel(float radius)
      glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
 }
 
-void create_grid()
+void create_grid(GLuint MyShader)
 {
     static const GLfloat g_vertex_buffer_data[] = {
         -100.0f,0.0f,100.0f, // triangle 1 : begin
@@ -216,6 +272,16 @@ void create_grid()
         100.0f, 0.0f,-100.0f,
         100.0f, 0.0f,100.0f, // triangle 2 : end
     };
+
+    static const GLfloat g_color_buffer_data[] = {
+        0.0f,1.0f,0.0f, // triangle 1 : begin
+        0.0f,1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, // triangle 1 : end
+        0.0f, 1.0f,0.0f, // triangle 2 : begin
+        0.0f, 1.0f,0.0f,
+        0.0f, 1.0f,0.0f, // triangle 2 : end
+    };
+
     glGenVertexArrays(1, &VAOs[grid]);
     glBindVertexArray(VAOs[grid]);
     GLuint trianglebuffer;
@@ -225,6 +291,16 @@ void create_grid()
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+
+    glBindAttribLocation(MyShader, 0, "pos");
+    glBindAttribLocation(MyShader, 1, "Incolor");
 }
 
 void mouse_func(int x, int y)
--- vertex.vert
+++ vertex.vert
@@ -1,13 +1,14 @@
 #version 120
 #extension GL_ARB_draw_instanced : enable
 attribute vec3 pos;
+attribute vec3 Incolor;
 
 uniform mat4 MVP;
 varying vec4 color;
 
 void main(void)
 {
-    color = vec4(0.0);
+    color = vec4(Incolor.x, Incolor.y, Incolor.z, 1.0);
     gl_Position =MVP * vec4(pos.x, pos.y, pos.z, 1.0);
 }
 
